5bedb94ef7f2151342c8261270e8f93c652df59b
PeopleArtFactory <info@peopleartfactory.com>
2025-04-11 12:26:58 +0200

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
-import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+//import { mdsvex } from 'mdsvex';
+//import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static'
 
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
+	//preprocess: [vitePreprocess(), mdsvex()],
 	kit: {
-        adapter: adapter()
+        adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
     },
 	extensions: ['.svelte', '.svx']
 };
